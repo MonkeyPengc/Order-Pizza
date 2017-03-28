7ffa6a400fa36553bd05d93a26d698ca3d784011
Aaron Peng <pengcheng612@gmail.com>
2017-03-27 19:06:30 -0700

    improved db

--- client/sales.py
+++ client/sales.py
@@ -8,24 +8,29 @@ class SalesDB:
         self.CreateTable()
 
     def CreateTable(self):
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS orderInventory (customer_id INT, datestamp TEXT, value REAL)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS orderInventory (order_id INTEGER PRIMARY KEY AUTOINCREMENT, customer_id INT, datestamp TEXT, value REAL)')
 
-    def InsertOrder(self, id, order_value):
+    def InsertOrder(self, cid, order_value):
         date = datetime.datetime.today().strftime('%Y-%m-%d %H-%M-%S')
         
         try:
-            self.cursor.execute('INSERT INTO orderInventory (customer_id, datestamp, value) VALUES(?, ?, ?)', (id, date, order_value))
+            self.cursor.execute('INSERT INTO orderInventory (customer_id, datestamp, value) VALUES(?, ?, ?)', (cid, date, order_value))
         
         except sqlite3.ProgrammingError:
             self.conn = sqlite3.connect('order_inventory.db')
             self.cursor = self.conn.cursor()
+            self.cursor.execute('INSERT INTO orderInventory (customer_id, datestamp, value) VALUES(?, ?, ?)', (cid, date, order_value))
         
         finally:
             self.conn.commit()
             self.cursor.close()
             self.conn.close()
 
-    def GetNumOrder(self):
-        self.cursor.execute('SELECT COUNT (customer_id) FROM orderInventory')
-        return self.cursor.fetchone()[0]
+    def AllocateCustomerID(self):
+        self.cursor.execute('SELECT MAX (customer_id) FROM orderInventory')
+        
+        try:
+            return self.cursor.fetchone()[0] + 1
 
+        except TypeError:
+            return 1
